873a10775b3939d12e974a8f09c0d46e55330eed
Emily Priddy <emily.priddy09@gmail.com>
2019-07-15 10:35:15 -0500

    Change listing individual props accepted by Degree.jsx to just 'props'

--- src/components/Degree.jsx
+++ src/components/Degree.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export const Degree = ({degree, college, date, description}) => {
+export const Degree = (props) => {
 	return (
 		<div className="education margin-b-50">
-			<h4><b>{degree}</b></h4>
-			<h5 className="font-yellow"><b>{college}</b></h5>
-			<h6 className="font-lite-black margin-t-10">{date}</h6>
-			<p className="margin-tb-30">{description}</p>
+			<h4><b>{props.degree}</b></h4>
+			<h5 className="font-yellow"><b>{props.college}</b></h5>
+			<h6 className="font-lite-black margin-t-10">{props.date}</h6>
+			<p className="margin-tb-30">{props.description}</p>
 		</div>
 	)
 }
